0aab2fc4f308ea7fb537a9becafcaf263286e45d
Aizahzaman <79235506+Aizahzaman@users.noreply.github.com>
2022-02-21 15:34:07 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 objects=[];
 status="";
 function preload(){
-video=createVideo("video.mp4");
+
 }
 
 function setup(){
     canvas=createCanvas(500,400);
     canvas.center();
-    video.hide();
+    image=createCapture(VIDEO);
+    image.hide();
+    image.size(500,400);
 }
 
 function start(){
     objectdetector=ml5.objectDetector("cocossd",modelLoaded);
     document.getElementById("status").innerHTML="Status: Detecting Objects";
+    var object_name=document.getElementById("Input").value;
+}
+    
+function modelLoaded(){
+    console.log("model has loaded");
+    status=true;
+}
+
+function gotresults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
+
+function draw(){
+    image(image,0,0,500,400);
 }
